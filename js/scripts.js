01a7425faef4ba0be07bfc638d32910abe4f0b8b
leanalarcon <alarcon.l@live.com>
2021-05-10 20:11:44 -0300

    The corresponding first files have been uploaded to the Travel Expenses website.

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+
+
+
+let vacationCalc = document.getElementById('vacationCalc')
+
+    vacationCalc.addEventListener('submit', calcExpenses)
+
+function getValues() {
+    let destiny = document.getElementById('destiny').value,
+        budget = document.getElementById('budget').value,
+        acomodation = document.getElementById('acomodation').value,
+        transport = document.getElementById('transport').value,
+        food = document.getElementById('food').value;
+
+        return {destiny, budget, acomodation, transport, food}
+}
+
+function calcExpenses(event) {
+
+    event.preventDefault();
+
+    const {destiny, budget, acomodation, transport, food} = getValues();
+
+    let expenses = parseInt(acomodation) + parseInt(transport) + parseInt(food)
+    let balance = parseInt(budget) - parseInt(expenses)
+
+    UI(destiny, budget, balance)
+}
+
+function UI(destiny, budget, balance) {
+    let result = document.getElementById('result')
+    let dataPrint = document.createElement('div')
+
+    dataPrint.innerHTML = `
+        <div class="container-data row">
+            <div class="col s4">
+                <h6>${destiny}</h6>
+            </div>
+            <div class="col s4">
+                <h6>${budget}</h6>
+            </div>
+            <div class="col s4">
+                <h6>${balance}</h6>
+            </div>
+        </div>
+    `
+    result.appendChild(dataPrint)
+
+    reset();
+}
+
+function reset() {
+    document.getElementById('vacationCalc').reset()
+}
